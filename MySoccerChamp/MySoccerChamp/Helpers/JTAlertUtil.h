8b07dee45d136e5e82e93d206488af8bc43ec157
Jonas Tomaz <jonas.tomaz@concretesolutions.com.br>
2014-07-29 19:17:00 -0300

    Helpers para mostrar alerts

---
+++ MySoccerChamp/MySoccerChamp/Helpers/JTAlertUtil.h
@@ -0,0 +1,13 @@
+//
+//  JTAlertUtil.h
+//  MySoccerChamp
+//
+//  Created by Jonas Tomaz on 7/29/14.
+//  Copyright (c) 2014 Jonas Tomaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTAlertUtil : NSObject
+
+@end
---
+++ MySoccerChamp/MySoccerChamp/Helpers/JTAlertUtil.m
@@ -0,0 +1,13 @@
+//
+//  JTAlertUtil.m
+//  MySoccerChamp
+//
+//  Created by Jonas Tomaz on 7/29/14.
+//  Copyright (c) 2014 Jonas Tomaz. All rights reserved.
+//
+
+#import "JTAlertUtil.h"
+
+@implementation JTAlertUtil
+
+@end
